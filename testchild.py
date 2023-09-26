3d1374fa013cc9276a4f7566508dab0c65bb8afd
Stu Robertson <stu.robertson.development@gmail.com>
2023-09-26 16:29:50 +0100

    Create testchild.py
    adding test file to child branch

---
+++ testchild.py
@@ -0,0 +1,5 @@
+# Adding a new file in child branch
+def pointless_function():
+  print("This pointless function is in a pointless branch in a pointless project in a pointless repository")
+
+pointless_function()
