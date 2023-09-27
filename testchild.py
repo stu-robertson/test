92b7d4c1096267725fd1686ee42f699284fbf6c5
Stu <stu@imaginaryemail.com>
2023-09-27 15:21:44 +0100

    adding new file, changing an old one

--- testchild.py
+++ testchild.py
@@ -1,5 +1,5 @@
 # Adding a new file in child branch
-def pointless_function():
-  print("This pointless function is in a pointless branch in a pointless project in a pointless repository")
+def test_for_git():
+  print("This doesn't do anything useful. But one day you will write functions that do!")
 
-pointless_function()
+test_for_git()
